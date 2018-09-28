a600fc96e3f752b14ba5ddc70cd38048c9dd5fb4
Benjamin Mesing <benjamin.mesing@comdirect.de>
2018-09-28 11:38:00 +0200

    RX-Tutorial

--- RXSample/app/src/main/java/rx_sample/rx_sample/MainActivity.kt
+++ RXSample/app/src/main/java/rx_sample/rx_sample/MainActivity.kt
@@ -1,14 +1,19 @@
 package rx_sample.rx_sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.Observable
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rxClickButton: Button
+    private lateinit var label: TextView
+
 
     companion object {
         val TAG = MainActivity::class.simpleName
@@ -17,20 +22,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rxClickButton = findViewById<View>(R.id.rxClickButton) as Button
+        label = findViewById<View>(R.id.label) as TextView
     }
 
     override fun onStart() {
         super.onStart()
         Observable.just(1,2,3,4,5,6)
                 .filter { it % 2 == 0 }
+                .map { it * 2 }
                 .doOnNext{
-                    Log.d(TAG, "Clicked: $it");
+                    Log.d(TAG, "Value: $it");
                 }
+                // mark 1 (keine Auswertung)
                 .subscribe{
+                    label.text = it.toString()
                 }
 
-        //RxView.clicks(findViewById<View>(R.id.rxClickButton)).subscribe{
-        //    Log.d(TAG, "Clicked");
-        //}
+        // mark 2 (UI Cool)
+        RxView.clicks(findViewById<View>(R.id.rxClickButton)).subscribe{
+            Log.d(TAG, "Clicked")
+        }
+
+        // mark 3 (WTF)
+        RxView.clicks(rxClickButton).subscribe{
+            Log.d(TAG, "Clicked2")
+        }
+
+        // mark 4 (Lösung: Hot vs. Cold)
+        val observable = RxView.clicks(rxClickButton).share();
+
+        observable.subscribe{
+            Log.d(TAG, "Clicked3")
+        }
+        observable.subscribe{
+            Log.d(TAG, "Clicked4")
+        }
+
+        /// Prinzip: UI-Änderungen sind Nebenwirkung von Event-Streams
+
+
+
+
+
     }
 }
